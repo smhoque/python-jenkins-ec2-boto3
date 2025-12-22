3e6b907ef6199b16d4e29acda563b6e585b765e3
emdadripon <eriponctg@gmail.com>
2025-12-22 18:40:42 -0500

    python file added

---
+++ ec2instanceboto3.py
@@ -0,0 +1,48 @@
+import boto3
+
+ec2_resource = boto3.resource('ec2')
+
+user_data_script = """#!/bin/bash
+apt update -y
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
+
+echo "<html><body><h1>My First EC2 Instance</h1></body></html>" > /var/www/html/index.html
+
+ufw allow 'Apache Full' || true
+"""
+
+instances = ec2_resource.create_instances(
+    ImageId='ami-0c55b159cbfafe1d7',  # Ubuntu AMI (verify region)
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+
+    BlockDeviceMappings=[
+        {
+            'DeviceName': '/dev/sda1',
+            'Ebs': {
+                'VolumeSize': 20,
+                'VolumeType': 'gp2',
+                'DeleteOnTermination': True
+            }
+        }
+    ],
+
+    UserData=user_data_script,
+
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'Ec2bootingtestinng'},
+                {'Key': 'Department', 'Value': 'Technical'},
+                {'Key': 'Environment', 'Value': 'Test'}
+            ]
+        }
+    ]
+)
+
+for instance in instances:
+    print(f"Instance ID: {instance.id} launched with a 20GB volume and Apache server.")
